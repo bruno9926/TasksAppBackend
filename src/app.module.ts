ba44803e31ac76939eec887f1f9c5e7eecbb87c3
Bruno <david.lopez9926@gmail.com>
2025-04-18 12:58:17 -0500

    basic config for db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import TasksModule from './tasks/tasks.module'; 
+// database connection
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TasksModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [],
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      }
+    }),
+    TasksModule
+  ],
   controllers: [],
   providers: [],
 })
